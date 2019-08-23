2d8d170c77d3f499e2b4907b39c3e74e50b9df44
GelenMR <gelen.montilla.r@gmail.com>
2019-08-23 12:31:00 -0400

    proyecto cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,30 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+
+  encode: (codigo2,escrito) => {
+      let msjcipher = "";
+   
+    for (let i = 0; i < escrito.length; i++) {
+
+        let cipher = escrito.charCodeAt(i);
+        let formula = (cipher - 65 + codigo2) % 26 + 65;
+        
+        let cipher2 = String.fromCharCode(formula);
+        msjcipher += cipher2;
+       
+    }
+    return msjcipher;
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (codigo2,escrito2) => {
+
+    let msj2cipher = "";
+
+    for (let i = 0; i < escrito2.length; i++) {
+
+        let descipher = escrito2.charCodeAt(i);
+        let formula2 = (descipher + 65 - codigo2) % 26 + 65;
+        let descipher2 = String.fromCharCode(formula2);
+        msj2cipher += descipher2;     
+    }
+    return msj2cipher;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-/*Acá va tu código*/
+
+const mensaje2 = document.getElementById("mensaje1");
+mensaje2.addEventListener("click", () => {
+
+    // hacer aparecer la segunda pantalla y desaparecer la primera
+
+    const divHidden = document.getElementById("mensajeria");
+    divHidden.style.display = "block";
+    const nonepag1 = document.getElementById("welcome");
+    nonepag1.style.display = "none";
+
+    // Escribir el nombre del usuario en segunda pantalla
+    const name2 = document.getElementById("nombre");
+    const name = document.getElementById("name").value;
+    name2.innerHTML = name;
+  
+
+
+});
+
+const encriptar = document.getElementById("enviarmensaje");
+encriptar.addEventListener("click", () => {
+
+
+    const divHidden2 = document.getElementById("encriptar");
+    divHidden2.style.display = "block";
+    const nonepag2 = document.getElementById("mensajeria");
+    nonepag2.style.display = "none";
+
+    let escrito = document.getElementById("escrito").value.toUpperCase();
+    let mensaje3 = document.getElementById("mensajeoculto");
+    let codigo2 = parseInt(document.getElementById("codigo").value);
+   
+    mensaje3.innerHTML = window.cipher.encode(codigo2, escrito);
+
+});
+const descifrado = document.getElementById("descifrar");
+descifrado.addEventListener("click", () => {
+
+    document.getElementById("escrito").value = "";
+
+    let escrito2 = document.getElementById("mensajeoculto").value.toUpperCase();
+    let mensaje4 = document.getElementById("mensajetraducido");
+    let codigo2 = parseInt(document.getElementById("codigo").value);
+
+    mensaje4.innerHTML = window.cipher.decode(codigo2,escrito2);    
+});
+
+const responder = document.getElementById("volver");
+responder.addEventListener("click", () => {
+   
+    document.getElementById("codigo").value = "";
+
+    const divHidden = document.getElementById("mensajeria");
+    divHidden.style.display = "block";
+    const divHidden2 = document.getElementById("encriptar");
+    divHidden2.style.display = "none";
+
+});
+
